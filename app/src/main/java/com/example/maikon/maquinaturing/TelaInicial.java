825ec0bd1206ebf5e84f2d4b6e8c0266d84fcf3b
Maikon Rodrigues <maikorodrigues787@gmail.com>
2019-07-10 19:01:28 -0300

    configurando tela inicial

---
+++ app/src/main/java/com/example/maikon/maquinaturing/TelaInicial.java
@@ -0,0 +1,26 @@
+package com.example.maikon.maquinaturing;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TelaInicial extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
